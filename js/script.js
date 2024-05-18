3d1c384643dc29b02514bf13124e2a9991696c35
taniash12 <aniashvili3@gmail.com>
2024-05-18 10:45:26 +0200

    added email validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 let navigationblock = document.getElementById("navBar");
-let buregrblock = document.getElementById("burgerBar")
+let buregrblock = document.getElementById("burgerBar");
 
-buregrblock.addEventListener("click",function() {
-    navigationblock.classList.toggle("active");
+buregrblock.addEventListener("click", function () {
+  navigationblock.classList.toggle("active");
+});
+
+document.getElementById("email").addEventListener("input", function () {
+  let email = document.getElementById("email").value;
+  let text = document.getElementById("text");
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern)) {
+    text.innerHTML = "თქვენი მეილი სწორია";
+    text.style.color = "#00ff00";
+  } else {
+    text.innerHTML = "თქვენი მეილი არასწორია";
+    text.style.color = "#D10F0F";
+  }
+  if (email == "") {
+    text.innerHTML = "";
+    text.style.color = "#D10F0F";
+  }
 });
